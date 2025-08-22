aaf888d84a70bbc248fa366f04ecc9d42f976798
xlocaste <irfandi.di122@gmail.com>
2025-08-22 21:15:12 +0700

    feat:landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 const App = () => {
   return (
-    <div className="bg-primary">
-      <h1 className='text-green-700'>Hello world</h1>
+    <div className="flex h-screen bg-gradient-to-tr from-secondary to-secondary items-center justify-center">
+      <div className="flex flex-col items-center">
+        <h1 className='text-primary italic text-2xl md:text-4xl'>Selamat Datang di</h1>
+        <p className="text-primary italic font-bold text-4xl md:text-7xl">Emie Boutique</p>
+      </div>
     </div>
   )
 }
